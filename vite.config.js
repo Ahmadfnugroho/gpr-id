534fb83734652794c218925525eceeb957998ae9
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-02-24 15:19:48 +0700

    perbaikan vite congif.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                // "resources/**/*.{blade.php,js}",
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/css/filament/admin/theme.css",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
